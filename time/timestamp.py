8fb8d503ce3a0e3e984ce1e3477538d84571f9fd
abounk <a_bunk911@hotmail.com>
2021-10-18 14:08:31 +0700

    refactor timestamp

--- time/timestamp.py
+++ time/timestamp.py
@@ -1,4 +1,4 @@
-package time
+import datetime
 """
 This code creates a datetime.time object from a string.
 
@@ -7,9 +7,8 @@ This code creates a datetime.time object from a string.
 - How would you modify it to be easier to read?
 """
 
-import datetime
 
-def createTimeFromTimestamp(timestamp: str) -> datetime.time:
+def create_time_from_timestamp(timestamp: str) -> datetime.time:
     """Create a datetime.time object from a string in the form 'hh:mm:ss'.
 
     Args:
@@ -31,7 +30,19 @@ def createTimeFromTimestamp(timestamp: str) -> datetime.time:
     args = timestamp.split(":")
     if len(args) != 3:
         raise ValueError('Timestamp must be "hh:mm:ss"')
-    else:
-        # if the timestamp is not valid, this may raise TypeError or ValueError
-        if 0 <= int(args[0]) <= 23 and 0 <= int(args[1]) < 60 and 0 <= int(args[2]) < 60:
-            return datetime.time(int(args[0]), int(args[1])), int(args[2]))
+    # Introduce Explanatory Variable
+    (hours, minutes, seconds) = args
+    # if the timestamp is not valid, this may raise TypeError or ValueError
+    if is_valid_time(hours, minutes, seconds):
+        return datetime.time(int(hours), int(minutes), int(seconds))
+    # otherwise the timestamp is invalid
+    return ValueError('Timestamp must be "hh:mm:ss"')
+
+
+def is_valid_time(hours, minutes, seconds):
+    """Verify the timestamp components are a valid time.
+
+    Raises:
+        ValueError if hours, minutes, seconds are not valid.
+    """
+    return 0 <= int(hours) <= 23 and 0 <= int(minutes) < 60 and 0 <= int(seconds) < 60
